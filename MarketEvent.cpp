d89fbbd9e4976583d9cdd82430c3f139676ba853
Alex <alexpetit12@hotmail.com>
2015-06-24 22:35:41 -0400

    Added MarketEvent.cpp

---
+++ MarketEvent.cpp
@@ -0,0 +1,14 @@
+/*
+ * \file MarketEvent.cpp
+ * \brief MarketEvent class source file
+ * \date 2015-06-24
+ */
+
+#include "MarketEvent.h"
+
+/*
+ * \brief MarketEvent empty constructor
+ */
+MarketEvent::MarketEvent()
+{
+};
