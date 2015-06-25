ae2149f1dc6be37df881aa30416a30aeee7b642c
Alex <alexpetit12@hotmail.com>
2015-06-24 22:34:57 -0400

    Added Event.h

---
+++ Event.h
@@ -0,0 +1,24 @@
+/**
+ * \file Event.h
+ * \brief Event class interface
+ * \date 2015-06-24
+ */
+
+#ifndef EVENT_H_
+#define EVENT_H_
+
+/**
+* \class Event
+* \brief Event is a base class
+*
+*     Event is a base which implements an interface
+*     for inherited events MarketEvent, SignalEvent,
+*     OrderEvent and FillEvent.
+*/
+class Event
+{
+};
+
+
+
+#endif /* EVENT_H_ */
