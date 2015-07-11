307824521d889e58d9574fb154c318a589f0e703
Alex <alexpetit12@hotmail.com>
2015-07-10 22:23:02 -0400

    Added updateBars method and an enum based on criterias

--- HistoricCSVDataHandler.cpp
+++ HistoricCSVDataHandler.cpp
@@ -7,34 +7,51 @@
 #include "HistoricCSVDataHandler.h"
 #include <boost/tokenizer.hpp>
 
+#include <cctype>
 #include <string>
-#include <typeinfo>
 
 
-HistoricCSVDataHandler::HistoricCSVDataHandler()
-:continueBacktest(true), barIndex(0)
+/*
+ * \brief HistoricCSVDataHandler constructor with parameters
+ * \param[in] events: queue of events
+ * \param[in] csvDir: path of directory containing csv files
+ * \param[in] symbolsList: list of symbols
+ */
+HistoricCSVDataHandler::HistoricCSVDataHandler(std::queue<Event> events,
+		std::string csvDir, std::vector<std::string>& symbolsVector)
+:events(events), csvDir(csvDir), symbolsVector(symbolsVector), continueBacktest(true), barIndex(0)
 {
+	openConvertCsvFiles();
 };
 
-HistoricCSVDataHandler::HistoricCSVDataHandler(std::queue<Event> events,
-		std::string csvDir, std::vector<std::string>& symbolsList)
-:events(events), csvDir(csvDir), symbolsList(symbolsList), continueBacktest(true), barIndex(0)
+/*
+ * \brief HistoricCSVDataHandler destructor
+ */
+HistoricCSVDataHandler::~HistoricCSVDataHandler()
 {
-	for(std::string& symbol : symbolsList)
+	for(std::string& symbol : symbolsVector)
+	{
+		delete fileStreams[symbol];
+	}
+};
+
+/*
+ * \brief openConvertCsvFiles method
+ */
+void HistoricCSVDataHandler::openConvertCsvFiles()
+{
+	for(std::string& symbol : symbolsVector)
 	{
 		std::string path = csvDir + symbol + ".csv";
 		fileStreams[symbol] = new std::ifstream(path);
 	}
 };
 
-HistoricCSVDataHandler::~HistoricCSVDataHandler()
-{
-	for(std::string& symbol : symbolsList)
-	{
-		delete fileStreams[symbol];
-	}
-};
-
+/*
+ * \brief getNewBar method
+ * param[in] symbol : the symbol for which the Bar is returned
+ * \return Returns a Bar object after having read data in the corresponding symbol's csv file
+ */
 Bar HistoricCSVDataHandler::getNewBar(std::string symbol)
 {
 	std::string csvLine;
@@ -47,6 +64,19 @@ Bar HistoricCSVDataHandler::getNewBar(std::string symbol)
 	std::vector<std::string> v(7);
 	v.assign(token.begin(), token.end());
 
-	Bar B(v[0], std::stof(v[1]), std::stof(v[2]), std::stof(v[3]), std::stof(v[4]), std::stoll(v[5]), std::stof(v[6]));
+	Bar B(v[Date], std::stof(v[Open]), std::stof(v[High]), std::stof(v[Low]), std::stof(v[Close]), std::stoll(v[Volume]), std::stof(v[AjdClose]));
 	return B;
+};
+
+Bar HistoricCSVDataHandler::getLatestBar(std::string symbol)
+{
+	return latestSymbolsData[symbol].back();
+};
+
+void HistoricCSVDataHandler::updateBars()
+{
+	for(std::string &symbol : symbolsVector)
+	{
+		latestSymbolsData[symbol].push_back(getNewBar(symbol));
+	}
 };
--- HistoricCSVDataHandler.h
+++ HistoricCSVDataHandler.h
@@ -16,22 +16,41 @@
 #include <string>
 #include <vector>
 
+
+/**
+* \class HistoricCSVDataHandler
+* \brief HistoricCSVDataHandler is an inherited class from class DataHandler
+*
+*     FillEvent manages filled order. It stores the quantity
+*     of the instrument, the price and the commission of the
+*     trade from the brokerage.
+*
+* Attributs:<br>
+* 	std::queue<Event> events: queue of events
+* 	std::string csvDir: path to directory containing csv files
+* 	std::vector<std::string> symbolsVector: vector containing symbols
+* 	std::map<std::string, std::vector<Bar>> latestSymbolsData: map containing each symbol's data
+* 	std::map<std::string, std::ifstream *> fileStreams: map containing each symbol's csv file ifstream
+*/
 class HistoricCSVDataHandler: public DataHandler
 {
 public:
-	HistoricCSVDataHandler();
 	HistoricCSVDataHandler(std::queue<Event> events, std::string csvDir,
-			std::vector<std::string>& symbolsList);
+			std::vector<std::string>& symbolsVector);
 
 	~HistoricCSVDataHandler();
 
-	Bar getNewBar(std::string symbol);
+	void openConvertCsvFiles();
 
-	std::vector<std::string> csvHeaders = {"Date", "Open", "High", "Low", "Close", "Volume", "AjdClose"};
+	Bar getNewBar(std::string symbol);
+	Bar getLatestBar(std::string symbol);
+	void updateBars();
+
+	enum csvHeaders {Date, Open, High, Low, Close, Volume, AjdClose};
 
 	std::queue<Event> events;
 	std::string csvDir;
-	std::vector<std::string> symbolsList;
+	std::vector<std::string> symbolsVector;
 	std::map<std::string, std::vector<Bar>> latestSymbolsData;
 	std::map<std::string, std::ifstream *> fileStreams;
 
